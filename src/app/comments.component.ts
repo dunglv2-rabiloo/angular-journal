385cbe88d918c4853d9bd57db7e672d61c7b9d0e
dunglv <dunglv2@rabiloo.com>
2024-05-29 08:40:30 +0700

    dependency injection

--- src/app/comments.component.ts
+++ src/app/comments.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommentService } from './comments.service';
 
 @Component({
   selector: 'app-comments',
@@ -6,9 +7,17 @@ import { Component } from '@angular/core';
   template: `
     <h3>Comments</h3>
     <ul>
-      <li>Great!</li>
-      <li>Awesome</li>
+      @for (comment of comments; track $index) {
+        <li>{{ comment }}</li>
+      }
     </ul>
   `,
 })
-export class CommentsComponent {}
+export class CommentsComponent {
+  commentService = inject(CommentService);
+  comments: string[];
+
+  constructor() {
+    this.comments = this.commentService.getComments();
+  }
+}
