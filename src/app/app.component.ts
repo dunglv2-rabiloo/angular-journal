503652e6acaa4f50ae71c46f0c00aa57dec31b71
dunglv <dunglv2@rabiloo.com>
2024-06-18 09:35:21 +0700

    add searchable select of type autocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  Option,
+  SearchableSelectComponent,
+} from './searchable-select/searchable-select.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchableSelectComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-journal';
+  initial: Option = {
+    value: '5',
+    label: 'Option 5',
+  };
+
+  selectOption(option: Option) {
+    console.log('selected', option);
+  }
+
+  doFilter(keyword: string): Option[] {
+    const OPTS = [
+      {
+        value: '1',
+        label: 'Option 1',
+      },
+      {
+        value: '2',
+        label: 'Option 2',
+      },
+      {
+        value: '3',
+        label: 'Option 3',
+      },
+    ];
+
+    keyword = keyword.toLowerCase();
+    const filtered = OPTS.filter((opt) =>
+      opt.label.toLowerCase().includes(keyword)
+    );
+    return filtered;
+  }
 }
